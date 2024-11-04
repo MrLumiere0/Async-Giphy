208af25fbd7f8d9bbe2d28f38a4452cfe89ce0e0
Hashid <hashidlouis@gmail.com>
2024-11-04 12:57:53 -0500

    Created succssfull API call in getImages)

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+ async function getImages(query){
+    const url =  `https://api.giphy.com/v1/gifs/search?
+    api_key=${process.env.API_KEY}
+    &q=${query}
+    &limit=25
+    &offset=0
+    &rating=g
+    &lang=en
+    &bundle=messaging_non_clips`
+
+
+     const response = await fetch(url);
+     const data =  await response.json();
+     return(data)
+
+
+}
+
+console.log(getImages("love"))
